7140cca87e3010122a5b6891047f3eb20b915f63
quattromani <max@maxquattromani.com>
2014-10-01 22:30:57 -0600

    json structure and basic page setup

--- js/build/production.js
+++ js/build/production.js
@@ -1,7 +1,15 @@
-// Get JSON from Google Doc
-$.getJSON("http://cors.io/spreadsheets.google.com/feeds/list/1XctAuEKcLieeQp5VAqO2bhjEgnDptoXCrBTLMYLJy-o/od6/public/values?alt=json", function(data) {
-  //first row "title" column
-  console.log(data.feed.entry[0]['gsx$title']['$t']);
+$.getJSON('https://spreadsheets.google.com/feeds/list/1XctAuEKcLieeQp5VAqO2bhjEgnDptoXCrBTLMYLJy-o/od6/public/values?alt=json', function(data) {
+
+  for (i = 0; i < data.feed.entry.length; i++) {
+    var specialist = data.feed.entry[i]['gsx$specialist']['$t'];
+    var units = data.feed.entry[i]['gsx$units']['$t'];
+    var value = data.feed.entry[i]['gsx$value']['$t'];
+    var total = data.feed.entry[i]['gsx$total']['$t'];
+
+    tr = $('<tr/>');
+    tr.append('<td class="specialist">' + specialist + '</td><td>' + units + '</td><td>' + value + '</td><td class="total">' + total + '</td>');
+    $('table').append(tr);
+  }
 });
 
 /* ==========================================================================
