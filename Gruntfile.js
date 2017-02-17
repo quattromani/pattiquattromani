6e982a345fd9604b6184f8b10841a8bf4d09087b
quattromani <max@maxquattromani.com>
2017-02-17 09:42:37 -0700

    setup CHL page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,93 +1,100 @@
+'use strict';
+
 module.exports = function(grunt) {
 
-    // load all grunt tasks matching the `grunt-*` pattern
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
 
-      concat: {
-        dist: {
-          src: [
-          'js/*.js'
-          ],
-          dest: 'js/build/production.js',
-        }
-      },
+  concat: {
+    dist: {
+      src: [
+      'js/scripts/*.js'
+      ],
+      dest: 'js/production.js',
+    }
+  },
 
-      uglify: {
-        build: {
-          src: 'js/build/production.js',
-          dest: 'js/build/production.min.js'
-        }
-      },
+  uglify: {
+    build: {
+      src: 'js/production.js',
+      dest: 'js/production.min.js'
+    }
+  },
 
-      sass: {
-        dist: {
-          options: {
-            style: 'expanded'
-          },
-          files: [{
-            expand: true,
-            cwd: 'css/scss/partials',
-            src: ['*.scss'],
-            dest: 'css/partials',
-            ext: '.css'
-          }], 'main.css': 'main.scss',
-        },
-        dev: {
-          files: [{'css/main.css': 'css/scss/main.scss'}]
-        }
-      },
-
-      autoprefixer: {
-        prefix: {
-          src: 'css/main.css',
-          dest: 'css/main.css'
-        },
-      },
-
-      cssmin: {
-        combine: {
-          files: {
-            'css/main.min.css': [ 'css/main.css' ]
-          }
-        }
-      },
-
-      imagemin: {
-        dynamic: {
-          files: [{
-            expand: true,
-            cwd: 'images/orig_assets',
-            src: ['*.{png,jpg,gif}'],
-            dest: 'images'
-          }]
-        }
-      },
-
-      watch: {
-        options: {
-          livereload: true
-        },
-        scripts: {
-          files: ['js/*.js'],
-          tasks: ['concat', 'uglify'],
-        },
-        css: {
-          files: ['css/scss/globals/*.scss','css/scss/partials/*.scss','css/scss/theme/*.scss'],
-          tasks: ['sass', 'cssmin', 'autoprefixer'],
-        }
+  sass: {
+    dist: {
+      files: {
+        'css/main.css' : 'css/scss/main.scss'
       }
+    }
+  },
 
-    });
+  postcss: {
+    options: {
+      map: true,
+
+      processors: [
+        require('pixrem')(),
+        require('autoprefixer')({browsers: 'last 2 versions'}),
+        require('cssnano')()
+      ]
+    },
+    dist: {
+      files: {
+        'css/main.css': ['css/main.css']
+      }
+    }
+  },
+
+  criticalcss: {
+    custom: {
+      options: {
+        url: "http://localhost:4000",
+        outputfile: "_includes/critical.css",
+        filename: "css/main.css",
+        buffer: 800*1024
+      }
+    }
+  },
+
+  imagemin: {
+    dynamic: {
+      files: [{
+        expand: true,
+        cwd: 'images/orig_assets',
+        src: ['*.{png,jpg,gif}'],
+        dest: 'images'
+      }]
+    }
+  },
+
+  watch: {
+    options: {
+      livereload: {
+        port: 4000,
+        key: grunt.file.read('/Users/Q/Sites/livereload.key'),
+        cert: grunt.file.read('/Users/Q/Sites/livereload.crt'),
+        files: ['_site/**/*'],
+      }
+    },
+    scripts: {
+      files: ['js/*.js'],
+      tasks: ['concat', 'uglify'],
+    },
+    css: {
+      files: ['css/scss/**/*.scss'],
+      tasks: ['sass', 'postcss'],
+    }
+  }
+
+});
 
 grunt.loadNpmTasks('grunt-contrib');
 
-grunt.registerTask('build', ['concat', 'uglify', 'sass', 'autoprefixer', 'cssmin', 'imagemin', 'watch']);
-grunt.registerTask('dev', ['concat', 'uglify', 'sass', 'autoprefixer', 'cssmin', 'watch']);
-grunt.registerTask('js', ['concat', 'uglify', 'watch']);
-grunt.registerTask('css', ['sass', 'autoprefixer', 'cssmin', 'watch']);
-grunt.registerTask('default', ['watch']);
+grunt.registerTask('build', ['concat', 'uglify', 'sass', 'postcss', 'imagemin', 'watch']);
+
+grunt.registerTask('prod', ['concat', 'uglify', 'sass', 'postcss', 'criticalcss', 'imagemin', 'watch']);
 
 };
